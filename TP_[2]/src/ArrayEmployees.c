8effb613cf703d26dcadf017138d3fb51f45fe03
Yukio <yukikreczmer@gmail.com>
2021-10-16 21:48:13 -0300

    TP_2

--- TP_[2]/src/ArrayEmployees.c
+++ TP_[2]/src/ArrayEmployees.c
@@ -4,15 +4,6 @@
  *  Created on: 11 oct. 2021
  *      Author:
  */
-
-/** \brief To indicate that all position in the array are empty,
- * this function put the flag (isEmpty) in TRUE in all
- * position of the array
- * \param list Employee* Pointer to array of employees
- * \param len int Array length
- * \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
- *
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -37,7 +28,7 @@ int initEmployees(Employee* list, int len)
 	return retorno;
 }
 
-int BuscarIdLibre(Employee list[], int len)//retorna -1 si no encuentra lugar libre o el id del elemento libre
+int BuscarIdLibre(Employee list[], int len)
 {
 	int retorno=-1;
 	int i;
@@ -99,13 +90,6 @@ void printOneEmployee(Employee empleado)
 		printf("%04d %10s %20s %20.2f %15d\n", empleado.id, empleado.name, empleado.lastName, empleado.salary, empleado.sector);
 }
 
-/** \brief print the content of employees array
- *
- * \param list Employee*
- * \param length int
- * \return int
- *
- */
 int printEmployees(Employee* list, int len)
 {
 	int retorno=-1;
@@ -140,14 +124,6 @@ void DatosToAddEmployee(char toAddName[], char toAddLastName[], float* toAddSala
 	strcpy(toAddLastName,auxLastName);
 }
 
-/** \brief find an Employee by Id en returns the index position in array.
- *
- * \param list Employee*
- * \param len int
- * \param id int
- * \return Return employee index position or (-1) if [Invalid length or NULL
-pointer received or employee not found]
- */
 int findEmployeeById(Employee* list, int len,int id)
 {
 	int retorno=-1;
@@ -165,15 +141,6 @@ int findEmployeeById(Employee* list, int len,int id)
 	return retorno;
 }
 
-/** \brief Remove a Employee by Id (put isEmpty Flag in 1)
- *
- * \param list Employee*
- * \param len int
- * \param id int
- * \return int Return (-1) if Error [Invalid length or NULL pointer or if can't
-find a employee] - (0) if Ok
- *
- */
 int removeEmployee(Employee* list, int len, int id)
 {
 	int retorno=-1;
@@ -205,15 +172,6 @@ int HayEmpleadosEnAlta(Employee list[], int len)//Retorna -1 si no hay hay emple
 	return retorno;
 }
 
-/** \brief Sort the elements in the array of employees, the argument order
-indicate UP or DOWN order. (Ordena el array de empleados por apellido y sector de manera ascendente o descendente)
- *
- * \param list Employee*
- * \param len int
- * \param order int [1] indicate UP - [0] indicate DOWN
- * \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
- *
- */
 int sortEmployees(Employee* list, int len, int order)
 {
 	int retorno=-1;
--- TP_[2]/src/ArrayEmployees.h
+++ TP_[2]/src/ArrayEmployees.h
@@ -62,12 +62,26 @@ int printEmployees(Employee* list, int len);
 /// @param empleado
 void printOneEmployee(Employee empleado);
 
-
+/// @brief Genera Id automaticamente para todas las posiciones de la lista
+///
+/// @param list
+/// @param len
+/// @return retorna -1 si la lista es NULL o su tamaño no es mayor que 0. O 0 si esta ok
 int GenerarId(Employee list[], int len);
 
+/// @brief ordena las posiciones del array por su ID de manera ascendente
+///
+/// @param list
+/// @param len
+/// @return	retorna -1 si la lista es NULL o su tamaño no es mayor que 0. O 0 si esta ok
 int OrdenarPorID(Employee list[], int len);
 
-
+/// @brief Pide datos para agregar un nuevo empleado y los pasa por referencia
+///
+/// @param toAddName espacio en memoria para agregar el nombre
+/// @param toAddLastName espacio en memoria para agregar el apellido
+/// @param toAddSalary espacio en memoria para agregar el salario
+/// @param toAddSector espacio en memoria para agregar el sector
 void DatosToAddEmployee(char toAddName[], char toAddLastName[], float* toAddSalary, int* toAddSector);
 
 /** \brief find an Employee by Id en returns the index position in array.
@@ -102,10 +116,42 @@ indicate UP or DOWN order
  *
  */
 int sortEmployees(Employee* list, int len, int order);
+
+/// @brief  Se muestra una lista de empleados dados de alta y permite modificar Nombre, Apellido, Salario o Sector eligiendo el ID a modificar
+///
+/// @param list
+/// @param len
+/// @return retorna -1 si la lista es NULL o su tamaño no es mayor que 0. O 0 si esta ok
 int ModificarEmployee(Employee list[], int len);
+
+/// @brief Se muestra una lista de empleados dados de alta y permite dar de baja eligiendo el ID.
+///
+/// @param list
+/// @param len
+/// @return retorna -1 si la lista es NULL o su tamaño es menor o igual a 0. O 0 si esta ok
 int BajaEmployee(Employee list[],int len);
+
+/// @brief Pasa por referencia el salario total de los empleados, el promedio de salario por empleado y la cantidad de empleados que superan al promedio
+///
+/// @param list
+/// @param len
+/// @param refSalarioTotal
+/// @param refPromedioSalario
+/// @param refSuperanPromedioSalario
+/// @return retorna -1 si la lista es NULL o su tamaño no es mayor que 0. O 0 si esta ok
 int PromedioSalarios(Employee list[], int len, float* refSalarioTotal, 	float* refPromedioSalario, int* refSuperanPromedioSalario);
+
+/// @brief se le pasa por parámetro un retorno. si este mismo es "-1" se imprime el mensaje de error
+///
+/// @param retorno
+/// @param ErrorMensaje
 void ValidarRetorno(int retorno, char ErrorMensaje[]);
-int HayEmpleadosEnAlta(Employee list[], int len);//Retorna -1 si no hay hay empleados dados de alta o 1 si hay
+
+/// @brief
+///
+/// @param list
+/// @param len
+/// @return retorna -1 si la lista es NULL, o su tamaño no es mayor que 0, o si no hay empleados dados de alta. O 1 si esta ok
+int HayEmpleadosEnAlta(Employee list[], int len);
 
 #endif /* ARRAYEMPLOYEES_H_ */
